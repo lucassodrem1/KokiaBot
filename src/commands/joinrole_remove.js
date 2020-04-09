8cde27dce55f43f06af0c82c915f691aedf4c479
Lucas Sodré <lucassodrem@gmail.com>
2020-04-09 08:44:40 -0300

    Comando joinrole remove.

---
+++ src/commands/joinrole_remove.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const GuildController = require('../controllers/Guild');
+
+exports.run = async (client, message, args) => {
+  // Verificar se usuário é um administrador.
+  if(!message.member.hasPermission('ADMINISTRATOR')) {
+    return message.channel.send('Você precisa ser um administrador para alterar a mensagem!');
+  }
+  
+  let guildController = new GuildController();
+
+  try {
+    //Verificar se existe joinrole.
+    let guildData = await guildController.getGuild(message.guild.id);
+    if(guildData.join_role === '0') {
+        return message.channel.send('Nenhuma auto role foi atribuida ainda!');
+    }
+
+    await guildController.updateInfo(message.guild.id, 'join_role', 0);
+
+    message.channel.send('Auto role removida!');
+  } catch(e) {
+    console.error(e);
+  }
+}
