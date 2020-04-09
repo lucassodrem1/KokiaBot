997b4bf5c96b883fb4924a9987e2650624b96aa6
Lucas Sodré <lucassodrem@gmail.com>
2020-04-09 07:57:13 -0300

    Comando joinrole set.

---
+++ src/commands/joinrole_set.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const GuildController = require('../controllers/Guild');
+
+exports.run = async (client, message, args) => {
+  // Verificar se usuário é um administrador.
+  if(!message.member.hasPermission('ADMINISTRATOR')) {
+    return message.channel.send('Você precisa ser um administrador para alterar a mensagem!');
+  }
+  
+  let role = message.mentions.roles.first();
+
+  if(!role) {
+    return message.channel.send('Você precisa escolher uma role!');
+  }
+  
+  let guildController = new GuildController();
+  await guildController.updateInfo(message.guild.id, 'joinrole', role.id);
+
+  message.channel.send('Auto role quando usuário entrar definida!');
+}
