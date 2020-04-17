8abffb1b861a73ab97808e828c806a475175e6d1
Lucas Sodré <lucassodrem@gmail.com>
2020-04-17 03:11:55 -0300

    Atualiazando dados caso o user upe usando o comando rank.

--- src/embeds/embedRank.js
+++ src/embeds/embedRank.js
@@ -1,4 +1,12 @@
 module.exports.embedRank = function(Discord, message, user) {
+    // Update nos valores do user para caso o mesmo upe
+    // usando o comando de rank.
+    if(user.current_xp_level >= user.nextXpLevel) {
+        user.level += 1;
+        user.current_xp_level -= user.nextXpLevel;
+        user.nextXpLevel = 5 * (Math.pow(user.level, 2)) + 50 * user.level + 100;
+    }
+    
     var embed = new Discord.MessageEmbed()
         .setTitle(user.displayName)
         .setDescription(`**Level:** ${user.level} \n**Exp:** ${user.current_xp_level} / ${user.nextXpLevel}\n**Rank:** ${user.ranking}º`)
