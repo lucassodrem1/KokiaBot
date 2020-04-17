ae77538d2bc0b8bbed4c1d781f7419ffa046951d
Lucas Sodré <lucassodrem@gmail.com>
2020-04-17 01:43:13 -0300

    Comando help.

---
+++ src/commands/help.js
@@ -0,0 +1,5 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  message.channel.send('Para aprender como eu funciono, acesse: https://lucassodrem.gitbook.io/kokiabot/');
+}
