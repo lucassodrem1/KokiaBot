a194a17da6246255790edd37f89ec1a51ded5ba0
Lucas Sodré <lucassodrem@gmail.com>
2020-04-22 16:04:59 -0300

    Anime search.

---
+++ src/commands/sranime.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const Jikan = require('jikan-node');
+const mal = new Jikan();
+const { embedSearchAnime } = require('../embeds/embedSearchAnime');
+const { formatAnimeGenre } = require('../utils/formatAnimeGenre');
+
+exports.run = async (client, message, args) => {
+  let genre = args[0];
+  let score = args[1];
+  
+  if(!genre) return message.channel.send('Você precisa escolher um gênero.');
+  if(!score) score = 0;
+  if(score < 0 || score > 10) return message.channel.send('Você precisa escolher um nota entre 0 e 10.');
+
+  genreNumber = formatAnimeGenre(genre.toLowerCase());
+  if(!genreNumber) {
+    let embed = new Discord.MessageEmbed()
+      .setDescription(`Esse gênero não existe. Veja os gêneros disponíveis [aqui](https://lucassodrem.gitbook.io/kokiabot/animes)!`);
+    return message.channel.send({embed: embed});
+  }
+  try {
+    let searchingMsg = message.channel.send(`Pesquisando animes de **${genre}** com nota mínima à **${score}**...`);
+    
+    let animesData = await mal.search('anime', '', {genre: genreNumber, score: score});
+    if(!animesData.results.length) return message.channel.send('Nenhum anime encontrado!'); 
+    
+    shuffleArray(animesData.results);
+    animesData.results.splice(20);
+    embedSearchAnime(Discord, message, animesData);
+    
+    searchingMsg.then(msg => msg.edit('Pesquisa concluída!'));
+  } catch(e) {
+    console.error(e);
+    message.channel.send('Ops, Algo deu errado! Pesquise novamente.');
+  }
+}
+
+// Embaralhar array.
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
---
+++ src/embeds/embedSearchAnime.js
@@ -0,0 +1,77 @@
+module.exports.embedSearchAnime = function(Discord, message, animesData) {
+  let actualPage = 1;
+  let pages = [];
+
+  // Divide 5 animes por página e adiciona em pages.
+  while(animesData.results.length) {
+    let page = animesData.results.splice(0, 5);
+    pages.push(page);
+  }
+
+  let embed = new Discord.MessageEmbed()
+    .setTitle('Animes encontrados:')
+    .setColor(0xf33434)
+    .setFooter(`Página ${actualPage} de ${pages.length}.`);
+
+  // Adicionando primeira página.
+  pages[actualPage - 1].forEach(anime => {
+    embed.addField('**Nome**', `[${anime.title}](${anime.url})`, true);
+    embed.addField('**Nota**', anime.score, true);
+    embed.addField('_ _', '_ _', false);
+  });
+
+  message.channel.send({embed: embed})
+  .then( msg => {
+    msg.react('⬅').then( r => {
+      msg.react('➡');
+
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && user.id === message.author.id;
+      
+      const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000});
+      const forwards = msg.createReactionCollector(forwardsFilter, {timer: 6000});
+      
+      // Reação ao ir para página anterior.
+      backwards.on('collect', r => {
+        if (actualPage === 1) return;
+        actualPage--;
+
+        // Editando página.
+        embed.fields = [];
+        pages[actualPage - 1].forEach(anime => {
+          embed.addField('**Nome**', `[${anime.title}](${anime.url})`, true);
+          embed.addField('**Nota**', anime.score, true);
+          embed.addField('_ _', '_ _', false);
+        });
+        embed.setFooter(`Página ${actualPage} de ${pages.length}.`);
+        msg.edit(embed);
+
+        // Removendo reação.
+        msg.reactions.removeAll().then(() => {
+          msg.react('⬅').then(() => msg.react('➡'));
+        });
+      });
+
+      // Reação ao ir para a próxima página. 
+      forwards.on('collect', r => {
+        if (actualPage === pages.length) return;
+        actualPage++;
+        
+        // Editando página.
+        embed.fields = [];
+        pages[actualPage - 1].forEach(anime => {
+          embed.addField('**Nome**', `[${anime.title}](${anime.url})`, true);
+          embed.addField('**Nota**', anime.score, true);
+          embed.addField('_ _', '_ _', false);
+        });
+        embed.setFooter(`Página ${actualPage} de ${pages.length}.`);
+        msg.edit(embed);
+
+        // Removendo reação.
+        msg.reactions.removeAll().then(() => {
+          msg.react('⬅').then(() => msg.react('➡'));
+        });
+      });
+    });
+  });   
+}
