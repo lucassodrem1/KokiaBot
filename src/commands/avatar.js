e57548cc305f2a73d18c90d6462162c24391c1b0
Lucas Sodré <lucassodrem@gmail.com>
2020-04-23 10:27:14 -0300

    Alteracao embed comando avatar.

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
   // Mostrar avatar do própria user se não escolher nenhum.
   if(!args[0]) {
     let embed = new Discord.MessageEmbed()
-      .setDescription(`Clique [aqui](${message.member.user.avatarURL()}) para baixar a imagem.`)
+      .setColor(0xf33434)
       .setImage(message.member.user.avatarURL());
 
     return message.channel.send({embed: embed});
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
   let member = message.guild.member(message.mentions.users.first());
 
   let embed = new Discord.MessageEmbed()
-    .setDescription(`Clique [aqui](${member.user.avatarURL()}) para baixar a imagem.`)
+    .setColor(0xf33434)
     .setImage(member.user.avatarURL());
 
   return message.channel.send({embed: embed});
