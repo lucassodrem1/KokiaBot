6d103d835f0b86b193b40a38648d9b8bafb77245
Lucas Sodré <lucassodrem@gmail.com>
2020-05-08 00:09:52 -0300

    Mostrar canal correio elegante em k!info.

--- src/embeds/embedGuildInfo.js
+++ src/embeds/embedGuildInfo.js
@@ -6,33 +6,36 @@ module.exports.embedGuildInfo = async function(Discord, message) {
   
   try {
     let guildData = await guildController.getGuild(message.guild.id);
-    let joinRole = message.guild.roles.cache.find(role => role.id === guildData.join_role);
-    let verifyRole = message.guild.roles.cache.find(role => role.id === guildData.verify_role);
+    let joinRole = guildData.join_role != 0 ? message.guild.roles.cache.find(role => role.id === guildData.join_role).name : 'OFF';
+    let verifyRole = guildData.verify_role != 0 ? message.guild.roles.cache.find(role => role.id === guildData.verify_role).name : 'OFF';
     let roleReplace = guildController.custom_role_replace ? 'ON' : 'OFF';
 
     let guildLevelSystem = await guildController.getGuildLevelSystem(message.guild.id);
     let levelUpChannel = 'default';
     if(guildLevelSystem.level_up_channel !== '0') {
-      levelUpChannel = message.guild.channels.cache.find(channel => channel.id === guildLevelSystem.level_up_channel);
-      levelUpChannel = levelUpChannel.name;
+      levelUpChannel = message.guild.channels.cache.find(channel => channel.id === guildLevelSystem.level_up_channel).name;
     }
 
     let guildWelcomeData = await guildController.getGuildWelcome(message.guild.id);
     let welcomeStatus = guildWelcomeData.status ? 'ON' : 'OFF';    
     let welcomeChannel = 'default';
     if(guildWelcomeData.channel !== '0') {
-      welcomeChannel = message.guild.channels.cache.find(channel => channel.id === guildWelcomeData.channel);
-      welcomeChannel = welcomeChannel.name;
+      welcomeChannel = message.guild.channels.cache.find(channel => channel.id === guildWelcomeData.channel).name;
+    }
+
+    let elegantMail = 'OFF';
+    if(guildData.elegant_mail_channel != 0) {
+      elegantMail = message.guild.channels.cache.find(channel => channel.id === guildData.elegant_mail_channel).name;
     }
 
     let embed = new Discord.MessageEmbed()
       .setTitle('Configurações no servidor.')
       .addField('**Prefixo**', `\`\`\`${guildData.prefix}\`\`\``, true)
-      .addField('**Auto Role**', `\`\`\`${joinRole.name}\`\`\``, true)
-      .addField('**Verify Role**', `\`\`\`${verifyRole.name}\`\`\``, true)
+      .addField('**Auto Role**', `\`\`\`${joinRole}\`\`\``, true)
+      .addField('**Verify Role**', `\`\`\`${verifyRole}\`\`\``, true)
       .addField('**Role Replace**', `\`\`\`${roleReplace}\`\`\``, true)
       .addField('**Boas-vindas**', `\`\`\`${welcomeStatus}\`\`\``, true)
-      .addField('_ _', '_ _', true)
+      .addField('**Correio elegante**', `\`\`\`${elegantMail}\`\`\``, true)
       .addField('**Canal de aviso de level**', `\`\`\`${levelUpChannel}\`\`\``, true)
       .addField('**Canal de boas-vindas**', `\`\`\`${welcomeChannel}\`\`\``, true)
       .addField('_ _', '_ _', true)
