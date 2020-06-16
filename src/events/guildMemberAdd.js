d6f63b4f5541f641a5e590d5f9bd53c3663cc376
Lucas Sodré <lucassodrem@gmail.com>
2020-06-16 17:12:54 -0300

    Avisar quando bot nao conseguir dar autorole.

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -6,6 +6,29 @@ const { embedWelcome } = require('../embeds/embedWelcome');
 module.exports = async (client, member) => {
   let userController = new UserController();
   let guildController = new GuildController();
+  
+  // Pegar primeiro canal de texto que encontrar
+  // para mandar mensagem de boas-vindas.
+  let channel = member.guild.channels.cache.find(channel => channel.type === 'text');
+
+  try {
+    let guildWelcomeData = await guildController.getGuildWelcome(member.guild.id);
+    // Mostrar mensagem de boas-vindas caso estiver ativada no servidor.
+    if(guildWelcomeData.status) {
+      // Alterar canal de texto para o que o usuário escolheu.
+      if(guildWelcomeData.channel !== '0') {
+        let customChannel = member.guild.channels.cache.find(channel => channel.id == guildWelcomeData.channel);
+        if(customChannel) {
+          channel = customChannel;
+        }
+      }
+      
+      // Enviar mensagem de boas-vindas.
+      // embedWelcome(Discord, member, channel, guildWelcomeData);
+    }
+  } catch(e) {
+    console.log(`Erro ao mostrar embed.\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e);
+  }
 
   try {
     let result = await userController.checkIfUserExists(member.guild.id, member.id)
@@ -18,29 +41,12 @@ module.exports = async (client, member) => {
     let guildData = await guildController.getGuild(member.guild.id);
     if(guildData.join_role !== '0') {
       let role = member.guild.roles.cache.find(role => role.id === guildData.join_role);
-      member.roles.add(role).catch(e => console.log(`Erro: Não tem permissão pra dar role!\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e));
+      member.roles.add(role).catch(e => {
+        console.log(`Erro: Não tem permissão pra dar role!\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e);
+        channel.send(`Kokia não pôde dar a autorole **${role.name}** por falta de permissões!`);
+      });
     }
   } catch(e) {
-    console.log(`Erro ao dar auto role.\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e);
-  }
-
-  try {
-    let guildWelcomeData = await guildController.getGuildWelcome(member.guild.id);
-    // Mostrar mensagem de boas-vindas caso estiver ativada no servidor.
-    if(guildWelcomeData.status) {
-      // Identificar canal ao qual será exibidor a mensagem.
-      let channel = member.guild.channels.cache.find(channel => channel.type === 'text');
-      if(guildWelcomeData.channel !== '0') {
-        let customChannel = member.guild.channels.cache.find(channel => channel.id == guildWelcomeData.channel);
-        if(customChannel) {
-          channel = customChannel;
-        }
-      }
-      
-      // Enviar mensagem de boas-vindas.
-      embedWelcome(Discord, member, channel, guildWelcomeData);
-    }
-  } catch(e) {
-    console.log(`Erro ao mostrar embed.\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e);
+    console.log(`Erro ao dar autorole.\n Evento: guildMemberAdd.\n Server: ${member.guild.name}\n`, e);
   }
 }
