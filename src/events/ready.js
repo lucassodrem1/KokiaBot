da74786d6333a19f51578405711079f2f6b25fbc
Lucas Sodré <lucassodrem@gmail.com>
2020-07-02 11:19:08 -0300

    Mudar status bot.

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,20 +4,9 @@ const { checkYoutube } = require('../utils/social/checkYoutube');
 
 module.exports = async (client) => {
   console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-  
-  let activitiesLength = Object.keys(client.config.activities).length;
-  let randomNumber = Math.floor(Math.random() * activitiesLength);
-  let typeActivity = client.config.activities[randomNumber].type;
-  client.user.setActivity(client.config.activities[randomNumber].quote, {type: typeActivity})
-    .catch(e => console.error('Error', e));
 
   // Alter status do bot.
-  setInterval(() => {
-    randomNumber = Math.floor(Math.random() * activitiesLength);
-    let typeActivity = client.config.activities[randomNumber].type;
-    client.user.setActivity(client.config.activities[randomNumber].quote, {type: typeActivity})
-      .catch(e => console.error('Error', e));
-  }, 120000);
+  client.user.setActivity('Kokia: https://bit.ly/kokiabot', {type: 'Watching'})
   
   // Verificar social a cada 3 segundos.
   setInterval(async () => {    
