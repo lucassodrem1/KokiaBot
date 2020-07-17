7b1f677c53e9125c936b091dd109fb1db603c975
Lucas Sodré <lucassodrem@gmail.com>
2020-07-17 00:19:11 -0300

    Mudança de texto.

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -16,7 +16,7 @@ module.exports = (client, guild) => {
       .setColor(0xf33434)
       .setAuthor(client.user.username, client.user.displayAvatarURL())
       .setThumbnail(client.user.displayAvatarURL())
-      .setDescription(`Oi! Meu prefixo é **${client.config.prefix}**. Digite **${client.config.prefix}help** para me conhecer!\n Se gostar, [vote em mim para me ajudar!](https://top.gg/bot/695267877892259890/vote)`)
+      .setDescription(`Oi! Meu prefixo é **${client.config.prefix}**. Digite **${client.config.prefix}help** para me conhecer!\n\n Se gostar, [vote em mim para me ajudar!](https://top.gg/bot/695267877892259890/vote)`)
 
   message.channel.send({embed: embed});
   // Salvar id da guild no db.
