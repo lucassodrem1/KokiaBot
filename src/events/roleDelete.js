38a14c8010a0898245f41cf3fbd2aedb0a4c1b1f
Lucas Sodré <lucassodrem@gmail.com>
2020-08-01 01:53:14 -0300

    Commit belongs to the previous.

--- src/events/roleDelete.js
+++ src/events/roleDelete.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const GuildController = require('../controllers/Guild');
-const GuildLolController = require('../controllers/GuildLol');
+
 // Evento para trocar salas se estiverem setadas no banco de dados para algo.
 module.exports = async (client, role) => {
   let guildController = new GuildController();
@@ -30,15 +30,6 @@ module.exports = async (client, role) => {
     });
 
     if(isCustomLevel) await guildController.deleteCustomLevelsByLevel(role.guild.id, isCustomLevel.level);
-
-    // Verificar se a role está no lol_maestry_roles.
-    let guildLolController = new GuildLolController(role.guild.id);
-    let guildMaestryRoles = await guildLolController.getRoles();
-    let isMaestryRole = guildMaestryRoles.find(maestryRole => {
-      return maestryRole.role == role.id;
-    });
-
-    if(isMaestryRole) await guildLolController.deleteMaestryRoleByPoints(isMaestryRole.points);
   } catch(e) {
     console.log(`Erro ao deletar role.\n Evento: roleDelete.\n Server: ${role.guild.name}\n`, e);
   }
