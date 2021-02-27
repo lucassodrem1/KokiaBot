0cbf014acc11d046ef4259f640637458173c522d
Lucas Sodré <lucassodrem@gmail.com>
2021-02-27 09:04:25 -0300

    [FIX] Bug when guild is unvailable.

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
-const GuildController = require('../controllers/Guild');
+const GuildController = require("../controllers/Guild");
 
 module.exports = (client, guild) => {
   // Verificar disponibilidade do server.
-  if(!guild.available) {
-    return channel.send('Server indisponível');
-  }
+  if (!guild.available) return;
 
   // Remover informações do server do db.
   let guildController = new GuildController();
   guildController.deleteGuild(guild.id);
-}
+};
